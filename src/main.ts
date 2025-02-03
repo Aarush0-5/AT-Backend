92dd7dee7ac315c9df59043a166167e46ba4244f
Aarush0-5 <singhaar0110@gmail.com>
2025-02-03 20:27:59 +0530

    CORS chanaged

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config()
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://amiteshtutorials.netlify.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   })
   app.setGlobalPrefix('api')
