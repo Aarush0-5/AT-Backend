526b0c65c60a87e3b2d44ce1d8e126137635d4b0
Aarush0-5 <singhaar0110@gmail.com>
2025-08-29 02:36:26 +0530

    Login for quiz updated

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports : [DatabaseModule] ,
