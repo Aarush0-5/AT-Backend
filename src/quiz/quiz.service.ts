fb5ea3ef1bb0870d4574058292c56c194dc060be
Aarush0-5 <singhaar0110@gmail.com>
2025-08-29 01:08:22 +0530

    security fixed

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenerativeAI } from '@google/genai';
 
 @Injectable()
 export class QuizService {
