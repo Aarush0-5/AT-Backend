a81e059dcf03413904852b1dcd6d36461b3cb514
Aarush0-5 <singhaar0110@gmail.com>
2025-08-29 04:24:45 +0530

    route changed in quiz

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { JwtAuthGuard } from 'src/authguard/jwtauth.guard';
+import { AuthModule } from 'src/authguard/jwtauth.module';
 
 @Module({
-  imports : [DatabaseModule] ,
+  imports : [DatabaseModule, AuthModule] ,
   controllers: [QuizController],
-  providers: [QuizService],
+  providers: [QuizService, JwtAuthGuard],
 })
 export class QuizModule {}
